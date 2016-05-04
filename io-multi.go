24ffb571aee53d429e9256c2cb35a56e691cf3e2
unsacrificed <unsacrificed@wwww4.ru>
2016-05-04 14:35:36 +0300

    Fix function declaration.

--- io-multi.go
+++ io-multi.go
@@ -20,7 +20,7 @@ func Read(r io.Reader, data ...[]byte) (int, error) {
 // Write is io.Writer.Write but with variable number of arguments.
 // Write stops writing after first error.
 // Returns number of successfully wrote arguments and error.
-func Write(w io.Writer, data ...interface{}) (int, error) {
+func Write(w io.Writer, data ...[]byte) (int, error) {
 	for i, d := range data {
 		if _, err := w.Write(d); err != nil {
 			return i, err
